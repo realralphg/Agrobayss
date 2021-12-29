93ac1ab66460736429ffc980d3457cdc4f55a160
Isah Raphael <33344810+realralphg@users.noreply.github.com>
2021-12-29 22:37:29 +0100

    First Commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>
+      Agrobays!
+    </h1>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
